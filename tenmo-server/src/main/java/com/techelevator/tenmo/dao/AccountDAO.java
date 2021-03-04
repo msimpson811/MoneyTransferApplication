428f64f63830f6eea079c913dcbc03f9ac2f6ccd
Mark Simpson <msimpson811@gmail.com>
2021-03-04 14:03:09 -0500

    account model, DAO, JDBC added, removed methods from TransferSqlDAO

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDAO.java
@@ -0,0 +1,19 @@
+package com.techelevator.tenmo.dao;
+
+import java.math.BigDecimal;
+
+import com.techelevator.tenmo.model.Account;
+
+public interface AccountDAO {
+
+	
+	
+	Account getBalance(int userId);
+	
+	BigDecimal withdraw(int fromUserId, BigDecimal amount);
+	
+	BigDecimal add(int fromUserId, BigDecimal amount);
+	
+	
+	
+}
