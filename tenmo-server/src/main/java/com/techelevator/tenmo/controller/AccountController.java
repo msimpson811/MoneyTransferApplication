21225239164cf30fc11d2fe5394dd0b8ac108578
Mike Vay <mikevay@gmail.com>
2021-03-04 22:04:31 -0500

    created custom exception, added request body and response status to controllers

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -3,12 +3,14 @@ package com.techelevator.tenmo.controller;
 import java.math.BigDecimal;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.tenmo.dao.AccountDAO;
+import com.techelevator.tenmo.exceptions.InsufficientBalanceException;
 import com.techelevator.tenmo.model.Account;
 @RestController
 //@PreAuthorize("isAuthenticated()")
@@ -25,14 +27,14 @@ public class AccountController {
 		return accountDao.getBalance(userId);
 	}
 	
-	@RequestMapping(path = "/withdraw", method = RequestMethod.POST)
-	public BigDecimal withdraw(@RequestParam int fromUserId, @RequestParam BigDecimal amount) {
+	@RequestMapping(path = "/withdraw", method = RequestMethod.PUT)
+	public BigDecimal withdraw(@RequestParam int fromUserId, @RequestParam BigDecimal amount, @RequestBody Account account) throws InsufficientBalanceException {
 		return accountDao.withdraw(fromUserId, amount);
 		
 	}
 	
-	@RequestMapping(path = "/receive", method = RequestMethod.POST)
-	public BigDecimal add (@RequestParam int toUserId, @RequestParam BigDecimal amount) {
+	@RequestMapping(path = "/receive", method = RequestMethod.PUT)
+	public BigDecimal add (@RequestParam int toUserId, @RequestParam BigDecimal amount, @RequestBody Account account) {
 		return accountDao.withdraw(toUserId, amount);
 	}
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -3,11 +3,14 @@ package com.techelevator.tenmo.controller;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techelevator.tenmo.dao.TransferDAO;
@@ -35,9 +38,9 @@ public class TransferController {
 		return transferDao.getTransferById(transferId);
 	}
 	
-	// fix this to include a request body!!!
+	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(path = "/transfer", method = RequestMethod.POST)
-	public void saveTransfer(@RequestParam int fromUserId, @RequestParam int toUserId, @RequestParam BigDecimal amount) {
+	public void saveTransfer(@RequestParam int fromUserId, @RequestParam int toUserId, @RequestParam BigDecimal amount, @RequestBody Transfer transfer) {
 		transferDao.transfer(fromUserId, toUserId, amount);
 	}
 }
